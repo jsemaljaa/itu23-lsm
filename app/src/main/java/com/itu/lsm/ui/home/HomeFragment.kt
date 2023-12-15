7feb23ceb3781992f92ebf9b232c7c3b393f45af
Alina Vinogradova <35191913+jsemaljaa@users.noreply.github.com>
2023-12-15 05:41:22 +0100

    backend connection done

--- app/src/main/java/com/itu/lsm/ui/home/HomeFragment.kt
+++ app/src/main/java/com/itu/lsm/ui/home/HomeFragment.kt
@@ -1,31 +1,28 @@
 package com.itu.lsm.ui.home
 
-import android.graphics.Rect
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.PagerSnapHelper
 import androidx.recyclerview.widget.RecyclerView
-import com.itu.lsm.R
 import com.itu.lsm.TaskAdapter
 import com.itu.lsm.databinding.FragmentHomeBinding
 
 import android.util.Log
+import androidx.lifecycle.ViewModelProvider
+import com.itu.lsm.InspirationAdapter
+import com.itu.lsm.classes.Task
 
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
-
-
     // ViewModel
-    private val homeViewModel by viewModels<HomeViewModel>()
+    private lateinit var homeViewModel: HomeViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -38,17 +35,35 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        homeViewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
+
+//        binding.rvTasks.apply {
+//            layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+//            adapter = taskAdapter
+//            PagerSnapHelper().attachToRecyclerView(this)
+//        }
+
+        //////////////////////////////////// OLD
         binding.rvTasks.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
         val taskAdapter = TaskAdapter(emptyList())
         binding.rvTasks.adapter = taskAdapter
 
         val snapHelper = PagerSnapHelper()
         snapHelper.attachToRecyclerView(binding.rvTasks)
+        //////////////////////////////////// OLD
 
         homeViewModel.tasks.observe(viewLifecycleOwner) { tasks ->
             taskAdapter.updateTasks(tasks)
             setupTabDots(tasks.size)
         }
+
+//        val adapter = InspirationAdapter(homeViewModel.inspirationList)
+//        binding.rvInspirationCards.adapter = adapter
+//
+//        viewModel.inspirationListLiveData.observe(viewLifecycleOwner, { list ->
+//            adapter.updateList(list)
+//        })
+
 //        Log.d("HomeFragment", "Item count: $itemCount")
 
         setupRecyclerViewScrollListener()
@@ -59,6 +74,12 @@ class HomeFragment : Fragment() {
         tabLayout.removeAllTabs()
         repeat(itemCount) {
             tabLayout.addTab(tabLayout.newTab())
+        }
+        for (i in 0 until tabLayout.tabCount) {
+            val tab = (tabLayout.getChildAt(0) as ViewGroup).getChildAt(i)
+            val layoutParams = tab.layoutParams as ViewGroup.MarginLayoutParams
+            layoutParams.setMargins(5, 0, 5, 0)
+            tab.requestLayout()
         }
     }
 
--- app/src/main/java/com/itu/lsm/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/itu/lsm/ui/home/HomeViewModel.kt
@@ -1,19 +1,44 @@
 package com.itu.lsm.ui.home
 
+import android.util.Log
+import androidx.fragment.app.FragmentManager.TAG
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
 import com.itu.lsm.classes.Task
+
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class HomeViewModel : ViewModel() {
 
-    private val _tasks = MutableLiveData<List<Task>>().apply {
-        value = listOf(
-            Task("WED, NOV 29 • 14:00", "English lesson"),
-            Task("FRI, DEC 15 • 11:00", "Cleaning"),
-            Task("SAT, DEC 16 • 09:00", "Cleaning")
-        )
+    private val database = Firebase.database
+    private val tasksRef = database.getReference("tasks")
+    private val _tasks = MutableLiveData<List<Task>>()
+    val tasks: LiveData<List<Task>> = _tasks
+
+    init {
+        loadTasks()
     }
 
-    val tasks: LiveData<List<Task>> = _tasks
+    private fun loadTasks() {
+        tasksRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val taskList = mutableListOf<Task>()
+//                for (snapshot in dataSnapshot.children) {
+//                    val task = snapshot.getValue(Task::class.java)
+//                    task?.let { taskList.add(it) }
+//                }
+                dataSnapshot.children.mapNotNullTo(taskList) { it.getValue<Task>(Task::class.java) }
+                _tasks.value = taskList
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                Log.w("loadPost:onCancelled", databaseError.toException())
+            }
+        })
+    }
 }
